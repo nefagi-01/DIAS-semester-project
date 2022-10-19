5430779505a089016951d856f7c522ccf53bd5d2
Stefan <stefanigescu@gmail.com>
2022-10-19 11:22:49 +0200

    Clean optimized version of kmeans and kmeans optimized

--- KMeans.py
+++ KMeans.py
@@ -1,87 +1,32 @@
 import time
 import numpy as np 
 
-class KMeans:
-    
-    def __init__(self, X, n_clusters=4):
-        self.X = X
-        self.K = n_clusters
-        self.centroids = X[np.random.choice(len(X), self.K, replace=False)]
-        self.intial_centroids = self.centroids
-        self.prev_label,  self.labels = None, np.zeros(len(X))
-        
-    def fit(self):
-        while not np.all(self.labels == self.prev_label) :
-            self.prev_label = self.labels
-            self.labels = self.predict(self.X)
-            self.update_centroid(self.X)
-        return self
-        
-    def predict(self, X):
-        return np.apply_along_axis(self.compute_label, 1, X)
+# Helpers
+def getLables(X, centroids, get_e=False):
+    diff = X[:, None] - centroids[None]  # (n, k, d)
+    dist = np.einsum('nkd,nkd->nk', diff, diff)
+    labels = dist.argmin(1)
+    if get_e:
+        dist.sort(1)
+        e = dist[:,1] - dist[:,0]
+        return labels, e
+    return labels
 
-    def compute_label(self, x):
-        return np.argmin(np.sqrt(np.sum((self.centroids - x)**2, axis=1)))
+def getCentroids(X, labels, k):
+    group_counts = np.bincount(labels, minlength=k)[:, None]
+    fn = lambda w: np.bincount(labels, weights=w, minlength=k)
+    return np.apply_along_axis(fn, 0, X) / group_counts
 
-    def update_centroid(self, X):
-        self.centroids = np.array([np.mean(X[self.labels == k], axis=0)  for k in range(self.K)])
-        
-        
-class KMeans_fast: 
-    def __init__(self, X, n_clusters=4):
-        self.X = X
-        self.K = n_clusters
-        self.N, self.D = X.shape
-        self.centroids = X[np.random.choice(len(X), self.K, replace=False)]
-        self.intial_centroids = self.centroids
-        self.prev_label,  self.labels = None, np.zeros(len(X))
-        # Expansion of numpy arrays in order to have only matrix operations for the assignment. No for loops
-        self.X_expanded = np.repeat(X[...,None],self.K,axis=2)
-        self.centroids_expanded =  np.repeat(self.centroids.T.reshape(1, self.D, self.K)[...,None], self.N,axis=0).reshape(self.X_expanded.shape)
-        
-    def fit(self, measure=False):
-        A_time = []
-        B_time = []
-        while not np.all(self.labels == self.prev_label) :
-            self.prev_label = self.labels
-            if measure:
-                start = time.time()
-            self.labels = self.predict()
-            if measure:
-                end = time.time()
-                A_time.append(end-start)
-                start = time.time()
-            self.update_centroid()
-            if measure:
-                end = time.time()
-                B_time.append(end-start)
-        if measure:
-            return np.array(A_time), np.array(B_time)
-        return
-    
-    def getClustersSize(self):
-        return np.array([self.X[self.labels == k].shape[0] for k in range(self.K)])
-        
-    def predict(self):
-        return np.argmin(np.linalg.norm(self.X_expanded - self.centroids_expanded, 2, 1), 1)
+def getCorrectedLables(X, centroids, speculated_centroids, e, labels):
+    D = np.max(np.nan_to_num(np.linalg.norm(centroids - speculated_centroids, 2, 1)))
+    mask_correction = (e - 2*D) <= 0
+    if mask_correction.sum() > 0:
+        labels[mask_correction] = getLables(X[mask_correction], centroids)
+    return labels
 
-    def update_centroid(self):
-        self.centroids = np.array([np.mean(self.X[self.labels == k], axis=0)  for k in range(self.K)])
-        # Expansion of numpy arrays in order to have only matrix operations for the assignment. No for loops
-        self.centroids_expanded =  np.repeat(self.centroids.T.reshape(1, self.D, self.K)[...,None],self.N,axis=0).reshape(self.X_expanded.shape)
-        
-        
-def KMeans_fast_refactor(X, k, num_iter=50, measure=False):
     
-    def getLables(X, centroids):
-        diff = X[:, None] - centroids[None]  # (n, k, d)
-        return np.einsum('nkd,nkd->nk', diff, diff).argmin(1)
-    
-    def getCentroids(X, labels, k):
-        group_counts = np.bincount(labels, minlength=k)[:, None]
-        fn = lambda w: np.bincount(labels, weights=w, minlength=k)
-        return np.apply_along_axis(fn, 0, X) / group_counts
-    
+# Implementations    
+def KMeans(X, k, num_iter=50, measure=False):
     n, d = X.shape
     centroids = X[np.random.choice(n, k, replace=False)]  # (k, d)
     if measure:
@@ -122,94 +67,37 @@ def KMeans_fast_refactor(X, k, num_iter=50, measure=False):
     return labels, centroids
 
 
+def KMeans_speculation(X, k, num_iter=50, subsample_size = 0.01, measure=False):    
+    # Data definition
+    n, d = X.shape
+    centroids = X[np.random.choice(n, k, replace=False)]
+    # Subsample of points used for speculation
+    mask = np.random.choice([True, False], size=X.shape[0], p=[subsample_size, 1-subsample_size])
+    
+    # Assignment step
+    labels = getLables(X, centroids)
+    for i in range(num_iter):
+        # Save previous labels
+        prev_labels = labels
+
+        # Speculate centroids using mask
+        # If some clusters are small, they may not be sampled. Therefore, update only centroids whose clusters have subsamples.
+        ma = np.ma.masked_invalid(getCentroids(X[mask], labels[mask], k))
+        centroids[~ma.mask] = ma.data[~ma.mask]
+        
+        # Assignment step, using the centroids before speculated
+        labels, e = getLables(X, centroids, get_e = True)
                 
+        # Before update step, save speculated_centroids for correction
+        speculated_centroids = centroids
         
-class KMeans_Speculation:
-    
-    def __init__(self, X, n_clusters=4, n_bins=5, subsample_size = 0.1, max_iterations = 1000):
-        self.X = X
-        self.K = n_clusters
-        self.N, self.D = X.shape
-        self.bins = np.linspace(0,30,n_bins)
-        self.centroids = X[np.random.choice(len(X), self.K, replace=False)]
-        self.speculated_centroids = X[np.random.choice(len(X), self.K, replace=False)]
-        self.prev_label,  self.labels = None, np.zeros(len(X))
-        self.subsample_size = subsample_size
-        # Expansion of numpy arrays in order to have only matrix operations for the assignment. No for loops
-        self.X_expanded = np.repeat(X[...,None],self.K,axis=2)
-        self.centroids_expanded =  np.repeat(self.centroids.T.reshape(1, self.D, self.K)[...,None], self.N,axis=0).reshape(self.X_expanded.shape)
-        self.speculated_centroids_expanded =  np.repeat(self.speculated_centroids.T.reshape(1, self.D, self.K)[...,None], self.N,axis=0).reshape(self.X_expanded.shape)
-        self.max_iterations = max_iterations
-
-    def fit(self, measure=False):
-        A_time = []
-        B_time = []
-        speculation_time = []
-        correction_time = []
-        # First assignment necessary to have speculation of centroids afterwards
-        self.labels = self.predict(self.X_expanded, self.centroids_expanded)
-        # Subsample of datapoints
-        self.mask = np.random.choice([True, False], size=self.X.shape[0], p=[self.subsample_size, 1-self.subsample_size])
-        counter = 0
-        while (not (self.labels == self.prev_label).all()) and counter < self.max_iterations:
-            self.prev_label = self.labels
-            if measure:
-                start = time.time()
-            self.speculate_centroid(self.X)
-            if measure:
-                end = time.time()
-                speculation_time.append(end-start)
-                start = time.time()
-            self.labels, self.e = self.predict(self.X_expanded, self.centroids_expanded, compute_e = True)
-            if measure:
-                end = time.time()
-                A_time.append(end-start)
-            # Save speculated centroids before updating it with correct value.
-            self.speculated_centroids = self.centroids
-            self.speculated_centroids_expanded = self.centroids_expanded
-            # Update centroids using all dataset
-            if measure:
-                start = time.time()
-            self.update_centroid(self.X, self.prev_label)
-            if measure:
-                end = time.time()
-                B_time.append(end - start)
-                start = time.time()
-            self.correct()
-            if measure:
-                end = time.time()
-                correction_time.append(end-start)
-            counter += 1
-        if measure:
-            return A_time, B_time, speculation_time, correction_time
-            
-        return self
-    
-    def speculate_centroid(self, X):
-        self.update_centroid(X[self.mask], self.labels[self.mask])
+        # Update step, using prev_labels (i.e: labels before update due to getLabels done on speculation)
+        centroids = getCentroids(X, prev_labels, k)
         
-    def predict(self, X_expanded, centroids_expanded, compute_e = False):
-        distances = np.linalg.norm(X_expanded - centroids_expanded, 2, 1)
-        labels = np.argmin(distances, 1)
-        if compute_e:
-            distances.sort(1)
-            e = distances[:,1] - distances[:,0]
-            return labels, e
-        return labels
-    
-    def correct(self):
-        digitized = np.digitize(self.e, self.bins)
-        D = np.max(np.nan_to_num(np.linalg.norm(self.centroids - self.speculated_centroids, 2, 1)))
-        mask = (self.e - 2*D) <= 0
-        #print(f'Number of corrected points: {mask.sum()}')
-        if mask.sum() > 0:
-            self.labels[mask] = self.predict(self.X_expanded[mask], self.centroids_expanded[mask])        
-
-    def update_centroid(self, X, labels):
-        self.centroids = np.array([np.mean(X[labels == k], axis=0)  for k in range(self.K)])
-        # Expansion of numpy arrays in order to have only matrix operations for the assignment. No for loops
-        self.centroids_expanded =  np.repeat(self.centroids.T.reshape(1, self.D, self.K)[...,None],self.N,axis=0).reshape(self.X_expanded.shape)
+        labels = getCorrectedLables(X, centroids, speculated_centroids, e, labels)
         
-    def getClustersSize(self):
-        return np.array([self.X[self.labels == k].shape[0] for k in range(self.K)])
+        # Check convergence
+        if i > 0 and (labels == prev_labels).all():
+            return labels, centroids
         
+    return labels, centroids
